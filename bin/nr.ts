93fdc703b9b42b7ed4ba15694af427d556342651
Ye Thu <yethu.personal@gmail.com>
2020-07-29 11:32:42 +0630

    Modified stack env

--- bin/nr.ts
+++ bin/nr.ts
@@ -4,4 +4,9 @@ import * as cdk from '@aws-cdk/core';
 import { NrStack } from '../lib/nr-stack';
 
 const app = new cdk.App();
-new NrStack(app, 'NrStack');
+new NrStack(app, 'NrStack', {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION,
+    },
+});
